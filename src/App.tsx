7978a5a9e378833a0cce2a7e86d4729ed972f465
Yukihiro <ruru.yukihiro@gmail.com>
2022-09-14 21:48:12 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className="bg-blue-50 min-h-screen">
         <div className="container mx-auto">
           <header className="flex justify-center items-center text-3xl h-32 mx-5">
-            いいねした画像を並べるサイト
+            いいねした画像を並べるサイト!!!
           </header>
           <div className="flex justify-center">
             <MainTable />
